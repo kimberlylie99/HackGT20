ff464be26a24dee0aa4eb8c4b8210468585801b4
Caleb Werth <cwerth@crimson.ua.edu>
2020-10-17 18:03:52 -0500

    String preprocessing

--- sus_highlight/text_input/views.py
+++ sus_highlight/text_input/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from .forms import TOSInputForm
+import re
 
 class IndexView(generic.FormView):
     template_name = 'text_input/index.html'
@@ -8,7 +9,22 @@ class IndexView(generic.FormView):
     success_url = reverse_lazy('text_input:highlight')
 
     def form_valid(self, form):
-        self.request.session['terms_of_service'] = form.cleaned_data['terms_of_service']
+        s = form.cleaned_data['terms_of_service']
+        print(s)
+        # Replace newlines with spaces
+        s = s.replace('\n', ' ')
+        # Replace tabs with spaces
+        s = s.replace('\t', ' ')
+        # Replace returns with spaces
+        s = s.replace('\r', ' ')
+        # Replace multiple spaces in a row with one space
+        s = re.sub(' +', ' ', s)
+        # Remove leading and trailing whitespace
+        s = s.strip()
+        # Split into list of strings on periods, but keep in the periods.
+        list_of_strings = re.findall('[^\.]+\.', s)
+        print(list_of_strings)
+        self.request.session['terms_of_service'] = s
         return super().form_valid(form)
 
 class HighlightView(generic.TemplateView):
