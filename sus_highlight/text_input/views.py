e4b152463dfa27ab07f1d1c5fe4a223e9112788c
Caleb Werth <cwerth@crimson.ua.edu>
2020-10-17 14:41:17 -0500

    Slight refactor and can print input

--- sus_highlight/text_input/views.py
+++ sus_highlight/text_input/views.py
@@ -1,11 +1,17 @@
 # from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 from .forms import TOSInputForm
 
 class IndexView(generic.FormView):
     template_name = 'text_input/index.html'
     form_class = TOSInputForm
-    success_url = 'highlight/'
+    success_url = reverse_lazy('text_input:highlight')
+
+    def form_valid(self, form):
+        terms_of_service = form.cleaned_data['terms_of_service']
+        print(terms_of_service)
+        return super().form_valid(form)
 
 class HighlightView(generic.TemplateView):
     template_name = 'text_input/highlight.html'
