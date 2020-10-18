5c2b45091184830e996d0a21bd1f1e640e36d680
shyam <shyamp229@gmail.com>
2020-10-17 21:44:50 -0400

    style applied

--- sus_highlight/text_input/forms.py
+++ sus_highlight/text_input/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class TOSInputForm(forms.Form):
-    terms_of_service = forms.CharField(widget=forms.Textarea)
+    terms_of_service = forms.CharField(widget=forms.Textarea(
+    		attrs={
+    		'class': 'form-control',
+    		'rows': '25'
+    		}
+    	))
